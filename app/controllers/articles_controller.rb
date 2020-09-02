d776493564ce7eba7cda771dbfb180313eccac0e
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2020-09-02 03:27:37 -0600

    add flash to create, edit and destroy methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,15 +16,24 @@ class ArticlesController < ApplicationController
       @article = Article.new(article_params)
       @article.save
       redirect_to article_path(@article)
+      flash.notice = "Article '#{@article.title}' was created succesfully!"
     end
     
     def destroy
       @article = Article.find(params[:id])
       @article.destroy
       redirect_to article_path(@article)
+      flash.notice = "Article '#{@article.title}' was deleted succesfully!"
     end
 
     def edit
       @article = Article.find(params[:id])
+    end
+
+    def update
+      @article = Article.find(params[:id])
+      @article.update(article_params)
+      flash.notice = "Article '#{@article.title}' Updated!"
+      redirect_to article_path(@article)
     end
 end
