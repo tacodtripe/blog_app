cd26f88d5be113551b12c03e809e07f792cec967
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2020-09-02 04:21:12 -0600

    add comment template

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@
 #   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 # end
 Blogger::Application.routes.draw do
-  resources :articles
+  root to: 'articles#index'
+  resources :articles do
+    resources :comments
+  end
 end
